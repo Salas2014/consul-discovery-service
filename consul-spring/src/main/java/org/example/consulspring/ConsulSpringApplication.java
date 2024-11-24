7a15e25d1e890dde47293ecd951b2c711daa6212
Vladyslav Hasporian <saslasengland33@gmail.com>
2024-11-24 12:59:31 +0200

    connect two service to consul agent for comunication for each ather

---
+++ consul-spring/src/main/java/org/example/consulspring/ConsulSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.consulspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulSpringApplication.class, args);
+	}
+
+}
---
+++ consul-spring/src/test/java/org/example/consulspring/ConsulSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.consulspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
